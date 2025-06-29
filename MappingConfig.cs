50c35e4a2ba9f7715b8cca1b78caeb5c947573f3
debyrise <deborahoby693@gmail.com>
2025-06-29 17:11:13 +0100

    completion of DI and autoMapper

---
+++ MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WebApiDemo.Model;
+using WebApiDemo.Model.Dto;
+
+namespace WebApiDemo
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDto>();
+            CreateMap<VillaDto, Villa>();
+
+            CreateMap<Villa, VillaCreateDto>().ReverseMap();
+            CreateMap<Villa, VillaUpdateDto>().ReverseMap();
+        }
+    }
+}
